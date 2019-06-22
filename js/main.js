a26d0aba19514404504eea770eb54128b9098a14
limonadiks <45362427+limonadiks@users.noreply.github.com>
2019-06-22 17:05:05 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -72,4 +72,13 @@ $(document).ready(function() {
 		}, 800);
 		return false;
 	});
+	
+	$(".nav--footer > a").click(function() {
+		var elementClick = $(this).attr("href")
+		var destination = $(elementClick).offset().top;
+		jQuery("html:not(:animated),body:not(:animated)").animate({
+			scrollTop: destination
+		}, 800);
+		return false;
+	});
 });
