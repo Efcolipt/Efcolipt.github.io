8c83c47e863e81d9fb87912e040858f798afcbbd
Efcolpit <45362427+Efcolpit@users.noreply.github.com>
2019-07-06 16:41:36 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -47,14 +47,7 @@ $(document).ready(function () {
 			    return false;
 		 });
 
-		  $(".menu--modal--content > ul > li > a").click(function() {
-			    var elementClick = $(this).attr("href")
-			    var destination = $(elementClick).offset().top;
-			    jQuery("html:not(:animated),body:not(:animated)").animate({
-			      scrollTop: destination
-			    }, 800);
-			    return false;
-		 });
+
 
 		  $('.menu--modal--content > ul > li > a ').click(function(){
 		  	 var menu = $(this).parent().parent().parent().parent().parent();
