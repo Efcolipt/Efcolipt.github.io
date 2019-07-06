7bd52a3c11520518ded72834736ad7d8b1e91c41
Efcolpit <45362427+Efcolpit@users.noreply.github.com>
2019-07-06 16:00:26 +0300

    upd

--- js/main.js
+++ js/main.js
@@ -62,6 +62,7 @@ $(document).ready(function () {
 			    nextArrow: '<img src="../img/Testimonials/arrow-right.png" class="slick-next" alt="arrow-right" style="position:absolute; top:50px; right:5px;" />',
 			    prevArrow: '<img src="../img/Testimonials/arrow-left.png" class="slick-prev" alt="arrow-left" style="position:absolute; top:50px; left:5px;" />'
 			  });
+			  $(".phone").mask("+7(999)999-99-99");
 		
 
 });
