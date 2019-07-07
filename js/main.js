9580cdd12f0377d23cb1c20f346804a3c682dc16
Efcolpit <45362427+Efcolpit@users.noreply.github.com>
2019-07-07 19:43:57 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -65,7 +65,8 @@ $(document).ready(function () {
 			    autoplaySpeed: 7000,
 			    nextArrow: '<img src="../img/Testimonials/arrow-right.png" class="slick-next" alt="arrow-right" style="position:absolute; top:50px; right:5px;" />',
 			    prevArrow: '<img src="../img/Testimonials/arrow-left.png" class="slick-prev" alt="arrow-left" style="position:absolute; top:50px; left:5px;" />',
-			    slideToScroll: 1
+			    slidesToShow: 2,
+  				slidesToScroll: 1
 			  });
 			  $(".phone").mask("+7(999)999-99-99");
 		
