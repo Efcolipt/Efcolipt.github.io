3083e5c508c83f1afbe7bb38fb6de426baf7d2af
Efcolpit <45362427+Efcolpit@users.noreply.github.com>
2019-07-07 19:41:06 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -61,11 +61,11 @@ $(document).ready(function () {
 
 		 
 			  $('.testimonial').slick({
-			    autoplay: false,
+			    autoplay: true,
 			    autoplaySpeed: 7000,
 			    nextArrow: '<img src="../img/Testimonials/arrow-right.png" class="slick-next" alt="arrow-right" style="position:absolute; top:50px; right:5px;" />',
 			    prevArrow: '<img src="../img/Testimonials/arrow-left.png" class="slick-prev" alt="arrow-left" style="position:absolute; top:50px; left:5px;" />',
-			    slidesToScroll:1
+			    slideToScroll: 1
 			  });
 			  $(".phone").mask("+7(999)999-99-99");
 		
