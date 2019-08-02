ef918fdbd0a592415ba1945b0ea704e39878d5ad
Efcolpit <45362427+Efcolpit@users.noreply.github.com>
2019-08-02 15:58:36 +0300

    upd

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-	$(document).on('mouseenter', '.this--person--img', function () {
-		$(this).find('.name--person').stop().fadeTo("500", 1);
-	}); 
-
-	$(document).on('mouseleave', '.this--person--img',function () {
-		$(this).find('.name--person').stop().fadeTo("500", 0);
-	});
-});
