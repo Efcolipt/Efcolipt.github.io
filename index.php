611bc044c9650a62185225a0e9f4d5e4099300d0
Efcolpit <45362427+Efcolpit@users.noreply.github.com>
2019-08-02 16:12:31 +0300

    upd

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="shortcut icon" href="img/shorcuticon70x70.jpg" type="image/png">
+
+	<meta property="og:url" content="" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Часы Apple Watch Edition Series 2" />
+	<meta property="og:image" content="img/shorcuticon70x70.jpg" />
+	<meta property="og:site_name" content="Мужские часы Apple Watch Edition Series 2" />
+	<meta property="og:description" content="Мужские часы Apple Watch Edition Series 2" />
+
+			<meta name="robots" content="noarchive" />
+	
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width">
+
+	<title>Мужские часы Apple Watch Edition Series 2</title>
+</head>
+<body>
+	<div class="topline">
+		<div class="wrapper">
+			<div class="df">
+			<h1 class="left">Apple Watch</h1>
+			<a href="#" class="right">Прием заказов <span>24/7</span></a>
+			</div>
+		</div>
+	</div>
+
+	<div class="Advertising ">
+		<div class="wrapper clearfix">
+			<div class="title">
+				<h1 class="fw700">Мужские часы Apple Watch  <span>Edition  Series 2</span></h1>
+				<h2><span>Со скидкой <span class="discount">67%</span></span></h2>
+				<p>Идеально подходят пунктуальным мужчинам с&nbsp;сильным характером.</p>
+			</div>
+			<div class="sale">
+				<div class="price clearfix">
+                <span class="price_old"><span class="price_1">5980</span><span class="price_curr">руб.</span></span>
+                <span class="price_new"><span class="price_2">2000</span><span class="price_curr">руб.</span></span><!-- after_qbici -->
+            </div>
+            <div class="btnHead clearfix">
+                <div class="left"><a href="#" class="button_order">Заказать со скидкой</a></div>
+                <div class="right">До конца акции:<br><span class="timer-action"><span class="timer-col"><strong>0</strong><span> д</span></span><span class="timer-col"><strong>13</strong><span> ч</span></span><span class="timer-col"><strong>02</strong><span> мин</span></span><span class="timer-col"><strong>33</strong><span> сек</span></span></span></div>
+            </div>
+			</div>
+		</div>
+	</div>
+	<div class="shop">
+		<div class="wrapper">
+			<ul class="sup-1 clearfix">
+            <li><img src="" alt="">
+                <p>Гарантия качества часов нашего интернет-магазина</p>
+            </li>
+            <li><img src="" alt="">
+                <p>Часы проходят тщательную проверку перед отправкой</p>
+            </li>
+            <li><img src="" alt="">
+                <p>Быстрая доставка почтой от 1 до 10 дней</p>
+            </li>
+            <li><img src="" alt="">
+                <p>Оплата заказа по факту получения на&nbsp;руки</p>
+            </li>
+        </ul>
+		</div>
+	</div>
+	<div class="slidebar">
+		<div class="wrapper">
+			<h2 class="overtitle">
+				<span>Apple Watch Edition Series 2</span>
+				 <span>– выбор настоящего мужчины!</span>
+			</h2>
+			<div class="photo-cont">
+            
+            	<img src="img/400x400.jpg" alt="часы Apple Watch Edition Series 2" width="300px" heigh="300px">
+            <img src="img/400x400 (1).jpg" alt="часы Apple Watch Edition Series 2" heigh="300px" width="300px">
+            <img src="img/400x400 (2).jpg" alt="часы Apple Watch Edition Series 2" width="300px" height="300px">
+            <img src="img/400x400 (4).jpg" alt="часы Apple Watch Edition Series 2" width="300px" height="300px">
+            
+        </div>
+		</div>
+	</div>
+</body>
+</html>
